b4ba03fe976800403b2003ed7ba1645d83888102
elizeuborges <elizeuborges@hotmail.com>
2016-04-21 22:42:26 -0300

    Ajustado nome do arquivo travis

--- src/test/java/br/com/javatools/xml/binder/XmlBinderTest.java
+++ src/test/java/br/com/javatools/xml/binder/XmlBinderTest.java
@@ -1,24 +1,24 @@
 package br.com.javatools.xml.binder;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-
-import java.net.URL;
 
 import org.junit.Test;
 
 public class XmlBinderTest {
 
+	private String xml = ""
+			+"<RootTag>"
+			+"	<Tag1>${alias.umMetodoQualquer()}</Tag1>"
+			+"	<Tag1>${(new Date() + 1).format('dd-MM-yyyy')}</Tag1>"
+			+"</RootTag>";
+	
 	public static String umMetodoQualquer(){
 		return "umValor";
 	}
 	
 	@Test
 	public void deveSetarSerPossivelInvocarMetodosApartirDoXml() {
-		URL xml = XmlBinderTest.class.getClassLoader().getResource("br/com/javatools/xml/binder/xml-test.xml");
-		assertThat(xml, is(notNullValue()));
 		XmlBinder binder = new XmlBinder(xml);
 		binder.bind("alias", this);
 		String xmlMontado = binder.getXml();
